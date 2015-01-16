98bb071ca3c4acc35050ac9871a3b90c93c88a67
James Kavanagh <james.kavanagh@datastax.com>
2015-01-16 08:58:17 -0800

    Fixed issues with logging

--- src/main/scala/org/kavanaghj/services/SparkServiceActor.scala
+++ src/main/scala/org/kavanaghj/services/SparkServiceActor.scala
@@ -35,11 +35,11 @@ trait SparkService extends HttpService with StatUtils with Logging {
       }
   } ~ path("mostPopularProduct") {
     get {
-      sqlCtx.logInfo("Received request to retrieve the most popular product")
+      logInfo("Received request to retrieve the most popular product")
       complete(getProductRatings(sqlCtx).toMap.maxBy { pr =>
         val (productId, (neg,pos)) = pr
         wilsonUpperBound(pos,(pos+neg),confidenceLevel)
-      })
+      }._1)
     }
 
   } ~ path("leastPopularProduct") {
@@ -47,28 +47,21 @@ trait SparkService extends HttpService with StatUtils with Logging {
       complete(getProductRatings(sqlCtx).toMap.minBy { pr =>
         val (productId, (neg,pos)) = pr
         wilsonLowerBound(pos,(neg+pos),confidenceLevel)
-      })
+      }._1)
     }
 
   }
 
-  private def getAggregates(ctx : CassandraSQLContext) : Array[(String,Double)] = {
-    ctx.sql("select product_id, avg(score), count(product_id) from reviews group by product_id")
-      .map {
-      x => (x.getString(0), x.getDouble(1))
-    }.collect()
-  }
-
   private def getProductRatings(ctx : CassandraSQLContext) : scala.collection.mutable.Map[String,(Int,Int)] = {
     val productScores = ctx.sql("select product_id, score from reviews order by product_id asc").map { x => (x.getString(0),x.getFloat(1)) }.collect()
-    ctx.logInfo(">>>> Retrieved list of products/scores. Processing into a map")
+    logInfo(">>>> Retrieved list of products/scores. Processing into a map")
     val map = scala.collection.mutable.Map.empty[String,(Int,Int)]
     productScores.foreach { x =>
       val (productId,score) = x
       val (neg : Int,pos : Int) = map.getOrElse(productId,(0,0))
       if(score >= 2.5) map.put(productId,(neg,pos+1)) else map.put(productId,(neg+1,pos))
     }
-    ctx.logInfo(">>>> Finished mapping of product and negative/positive scores")
+    logInfo(">>>> Finished mapping of product and negative/positive scores")
     map
   }
 
